0696c8bcfc4c9f7b44dd5f633f64f8e413ecacbf
aikhe <ikeandrie.ro@gmail.com>
2025-07-07 00:23:01 +0800

    feat(react-scan): installation & setup

---
+++ apps/site/components/ReactScan.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { scan } from 'react-scan/all-environments';
+import { JSX, useEffect } from 'react';
+
+export function ReactScan(): JSX.Element {
+  useEffect(() => {
+    scan({
+      enabled: true,
+    });
+  }, []);
+
+  return <></>;
+}
